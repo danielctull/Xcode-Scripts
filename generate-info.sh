9b2e9c171323a0de1f8222563211851a42e692ea
Daniel Tull <dt@danieltull.co.uk>
2015-01-16 10:46:52 +0000

    Add a script to generate an info precompiled header

---
+++ generate-info.sh
@@ -0,0 +1,40 @@
+cd "${PROJECT_DIR}"
+
+infoPlist="${SRCROOT}/${INFOPLIST_FILE}"
+infoPath="${infoPlist%.*}"
+infoName=$(basename "${infoPath}")
+infoHeader="${infoPath}.h"
+infoImplementation="${infoPath}.m"
+infoHeaderImport=$(basename "${infoHeader}")
+
+rm -f "$infoHeader"
+rm -f "$infoImplementation"
+touch "$infoHeader"
+touch "$infoImplementation"
+
+displayName=$(/usr/libexec/PlistBuddy -c "Print :CFBundleDisplayName" "$infoPlist")
+bundleIdentifier=$(/usr/libexec/PlistBuddy -c "Print :CFBundleIdentifier" "$infoPlist")
+bundleShortVersionString=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$infoPlist")
+bundleVersion=`git log --oneline | wc -l | tr -d ' '`
+
+echo "#define ${PROJECT_NAME}_Bundle_Version $bundleVersion" >> "$infoHeader"
+echo "" >> "$infoHeader"
+echo "#ifdef __OBJC__" >> "$infoHeader"
+echo "@import Foundation;" >> "$infoHeader"
+echo "" >> "$infoHeader"
+echo "extern const struct $infoName {" >> "$infoHeader"
+echo "	__unsafe_unretained NSString *displayName;" >> "$infoHeader"
+echo "	__unsafe_unretained NSString *bundleIdentifier;" >> "$infoHeader"
+echo "	__unsafe_unretained NSString *bundleVersion;" >> "$infoHeader"
+echo "	__unsafe_unretained NSString *bundleShortVersionString;" >> "$infoHeader"
+echo "} $infoName;" >> "$infoHeader"
+echo "#endif" >> "$infoHeader"
+
+echo "#import \"${infoHeaderImport}\"" >> "$infoImplementation"
+echo "" >> "$infoImplementation"
+echo "const struct $infoName $infoName = {" >> "$infoImplementation"
+echo "	.displayName = @\"$displayName\"," >> "$infoImplementation"
+echo "	.bundleIdentifier = @\"$bundleIdentifier\"," >> "$infoImplementation"
+echo "	.bundleVersion = @\"$bundleVersion\"," >> "$infoImplementation"
+echo "	.bundleShortVersionString = @\"$bundleShortVersionString\"" >> "$infoImplementation"
+echo "};" >> "$infoImplementation"
